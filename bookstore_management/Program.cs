f3a295456984b2650ddc309268864395a0eb75d0
Teomathe <trongducle080303@gmail.com>
2024-11-18 21:04:29 +0700

    Add project files.

---
+++ bookstore_management/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bookstore_management
+{
+    internal class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
